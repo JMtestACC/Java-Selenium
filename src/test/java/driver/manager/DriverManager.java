21852d768febefd01def79b9ceda887b1fa71f6f
Jan Miakisz <jan.miakisz@nokia.com>
2020-02-05 16:24:36 +0100

    JM#12: Building framework - Firefox and IE browsers handling

--- src/test/java/driver/manager/DriverManager.java
+++ src/test/java/driver/manager/DriverManager.java
@@ -1,10 +1,12 @@
 package driver.manager;
 
+import driver.BrowserFactory;
+import driver.BrowserType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverManager {
 
+    private static final BrowserType BROWSER_TYPE = BrowserType.CHROME;
     private static WebDriver driver;
 
     private DriverManager() {
@@ -12,15 +14,16 @@ public class DriverManager {
 
     public static WebDriver getWebDriver() {
         if(driver == null) {
-            System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
-            driver = new ChromeDriver();
+            driver = BrowserFactory.getBrowser(BROWSER_TYPE);
         }
         return driver;
     }
 
     public static void disposeDriver() {
         driver.close();
-        driver.quit();
+        if(!BROWSER_TYPE.equals(BrowserType.FIREFOX)) {
+            driver.quit();
+        }
         driver = null;
     }
 }
